8bd097eae2581376f0a3721d511a0e523d886944
Mifdha Milan <mifmilan@gmail.com>
2021-05-21 16:32:13 +0530

    event-binding

--- src/app/event-binding/event-binding.component.ts
+++ src/app/event-binding/event-binding.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-binding.component.css']
 })
 export class EventBindingComponent implements OnInit {
-
+  public call = "";
   constructor() { }
 
   ngOnInit() {
   }
-
+  callUser() {
+    console.log("welcome User");
+    this.call = "This is will pop up on button click";
+  }
+  getInfo(event) {
+    console.log(event);
+  }
 }
